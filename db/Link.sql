bcf74704e8b50acc14bccad697c35216255c762f
SadeeshaPerera <121984253+SadeeshaPerera@users.noreply.github.com>
2025-08-05 21:27:26 +0530

    fix: build issue

--- db/Link.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "Link" (
-    "name" VARCHAR(30) PRIMARY KEY NOT NULL,
-    "redirect" VARCHAR(500) NOT NULL,
-    "description" VARCHAR(250),
-    "pinned" BOOLEAN DEFAULT FALSE,
-    "createdBy" VARCHAR(50) NOT NULL,
-    "views" INT DEFAULT 0
-);
--- db/Link.ts
+++
@@ -1,8 +0,0 @@
-export default interface Link {
-  readonly name: string
-  readonly redirect: string
-  readonly description?: string
-  readonly pinned: boolean
-  readonly createdBy: string
-  readonly views: number
-}
