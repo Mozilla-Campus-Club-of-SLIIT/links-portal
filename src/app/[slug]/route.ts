7e357106ac218c82d0a98e5b16b94b37c4c08fa1
Seniru <senirupasan@gmail.com>
2025-09-01 11:55:57 +0530

    feat: redirections

---
+++ src/app/[slug]/route.ts
@@ -0,0 +1,22 @@
+import { redirect } from "next/navigation"
+
+// hardcoded list of redirects
+// todo: maybe use a database to dynamically find the routes
+const redirects = {
+  instagram: "https://instagram.com/sliitmozilla",
+  facebook: "https://facebook.com/sliitmozilla",
+  linkedin: "https://linkedin.com/company/sliitmozilla",
+  github: "https://github.com/Mozilla-Campus-Club-of-SLIIT",
+  youtube: "https://youtube.com/@sliitmozilla",
+  medium: "https://medium.com/@infosliitmcc",
+  twitter: "https://x.com/mozillasliit",
+  web: "https://www.sliitmozilla.org",
+  join: "https://forms.gle/1eNYuiNqd7CwbGEw6"
+} as Record<string, string>
+
+export async function GET(request: Request, { params }: { params: Promise<{ slug: string }> }) {
+  const slug = (await params).slug
+  if (!Object.hasOwn(redirects, slug)) return new Response("Redirect not found", { status: 404 })
+  const _redirect = redirects[slug]
+  return redirect(_redirect)
+}
